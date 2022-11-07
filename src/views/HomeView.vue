bb775d7a098c04f3fe648ab62810ca93e63fd460
David-Buchukuri <datobuchukuri20@gmail.com>
2022-11-08 03:00:13 +0400

    chore: fill homepage with some text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <template>
   <main>
-    <h1>Homepage</h1>
+    <h1>Homepage 💀️</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quasi
+      incidunt voluptatem quo harum magnam est modi ab repellendus consequatur
+      ipsum eveniet quis ad, tenetur hic vitae sint alias. Modi magnam saepe
+      voluptatibus vero nulla aliquam nisi vitae totam, cumque eos ipsam laborum
+      a consequuntur esse quibusdam, maiores necessitatibus pariatur blanditiis
+      expedita, dolorum iusto amet atque similique. Quam voluptate, explicabo
+      obcaecati labore alias, fugit, voluptas dignissimos sequi commodi
+      consequatur excepturi.
+    </p>
   </main>
 </template>
