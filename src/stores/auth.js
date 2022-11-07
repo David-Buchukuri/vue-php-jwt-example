7624f21e46b953faa1f02f23f6fe033f631511af
David-Buchukuri <datobuchukuri20@gmail.com>
2022-11-07 21:32:09 +0400

    feat: pinia store with auth state

---
+++ src/stores/auth.js
@@ -0,0 +1,7 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useAuthStore = defineStore("auth", () => {
+  const authenticated = ref(false);
+  return { authenticated };
+});
