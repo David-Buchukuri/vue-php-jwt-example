b0717f279b11f00cbd7988948eb046b803f7b078
David-Buchukuri <datobuchukuri20@gmail.com>
2022-11-08 02:43:33 +0400

    feat: logout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <nav>
-    <router-link to="/login"> login </router-link>
-    <router-link to="/register"> register </router-link>
-    <router-link to="/profile"> profile </router-link>
     <router-link to="/"> home </router-link>
+    <router-link to="/profile"> profile </router-link>
+    <router-link to="/register"> register </router-link>
+    <router-link to="/login"> login </router-link>
+    <button @click="logout" to="/">logout</button>
   </nav>
   <router-view />
 </template>
+
+<script setup>
+import axiosInstance from "@/config/axios";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const logout = async () => {
+  try {
+    axiosInstance.get("/logout");
+  } catch (err) {
+    console.log(err);
+  } finally {
+    router.push("/");
+  }
+};
+</script>
 
 <style scoped>
 nav {
@@ -17,5 +35,21 @@ nav {
 
 a {
   padding: 5px;
+}
+
+.router-link-exact-active {
+  background-color: hsla(160, 100%, 37%, 0.2);
+}
+
+button {
+  background: none;
+  border: none;
+  padding: 5px;
+  font-size: 20px;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s ease-out;
+  font-family: Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
 }
 </style>
