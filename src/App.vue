1989f975881a75f63866de4ee98b2318c4f51b2f
David-Buchukuri <datobuchukuri20@gmail.com>
2022-11-08 02:03:50 +0400

    feat: navbar

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
+  <nav>
+    <router-link to="/login"> login </router-link>
+    <router-link to="/register"> register </router-link>
+    <router-link to="/profile"> profile </router-link>
+    <router-link to="/"> home </router-link>
+  </nav>
   <router-view />
 </template>
+
+<style scoped>
+nav {
+  display: flex;
+  gap: 50px;
+  font-size: 20px;
+}
+
+a {
+  padding: 5px;
+}
+</style>
