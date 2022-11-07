bf81b2f90e88c8636c978b12ac235814ffa6d125
David-Buchukuri <datobuchukuri20@gmail.com>
2022-11-07 22:45:51 +0400

    chore: register pinia in main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { createPinia } from "pinia";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+const pinia = createPinia();
+app.use(pinia);
 
-app.mount('#app')
+app.use(router);
+
+app.mount("#app");
